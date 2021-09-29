85c2fdd1dbeb67661700f44f096d649ace82272d
kureta <skureta@gmail.com>
2021-09-30 01:07:17 +0300

    Moved code into `src`

--- Makefile
+++ Makefile
@@ -48,12 +48,12 @@ clean-test: ## remove test and coverage artifacts
 	rm -fr .pytest_cache
 
 lint/flake8: ## check style with flake8
-	flake8 rt_ddsp tests
+	flake8 src tests
 
 lint: lint/flake8 ## check style
 
 type-check:
-	mypy rt_ddsp/ tests/
+	mypy src tests
 
 test: ## run tests quickly with the default Python
 	pytest
@@ -64,7 +64,7 @@ test-tox: ## run tests on every Python version with tox
 	tox
 
 coverage: ## check code coverage quickly with the default Python
-	coverage run --source rt_ddsp -m pytest
+	coverage run --source src -m pytest
 	coverage report -m
 	coverage html
 	$(BROWSER) htmlcov/index.html
@@ -72,7 +72,7 @@ coverage: ## check code coverage quickly with the default Python
 docs: ## generate Sphinx HTML documentation, including API docs
 	rm -f docs/rt_ddsp.rst
 	rm -f docs/modules.rst
-	sphinx-apidoc -o docs/ rt_ddsp
+	sphinx-apidoc -o docs/ src
 	$(MAKE) -C docs clean
 	$(MAKE) -C docs html
 	$(BROWSER) docs/_build/html/index.html
--- setup.py
+++ setup.py
@@ -4,56 +4,5 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [
-    "wheel",
-    "torch",
-    "torchvision",
-    "torchaudio",
-    "pytorch-lightning",
-    "JACK-Client",
-    "librosa",
-    "numpy",
-    "scipy",
-    "python-osc",
-    "SoundFile",
-    "torchcrepe",
-]
-
-test_requirements = ['pytest>=3', ]
-
-setup(
-    author="Sahin Kureta",
-    author_email='skureta@gmail.com',
-    python_requires='>=3.9',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.9',
-    ],
-    description="Realtime DDSP implementation in PyTorch",
-    entry_points={
-        'console_scripts': [
-            'rt_ddsp=rt_ddsp.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='rt_ddsp',
-    name='rt_ddsp',
-    packages=find_packages(include=['rt_ddsp', 'rt_ddsp.*']),
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/kureta/rt_ddsp',
-    version='0.1.0',
-    zip_safe=False,
-)
+setup()
