a1431060e752ba29258b3629d7cfcce637fcd07b
kureta <skureta@gmail.com>
2021-11-16 14:03:03 +0300

    load from model.ckpt

--- src/zak/serialize.py
+++ src/zak/serialize.py
@@ -7,15 +7,16 @@ from zak.model import Decoder
 
 
 def load_checkpoint(version: int) -> OrderedDict:
-    file = Path(
-        Path.cwd(),
-        'lightning_logs',
-        'default',
-        f'version_{version}',
-        'checkpoints',
-    ).glob('*.ckpt')
-    file = sorted(list(file), key=lambda x: int(x.name.split('-')[0].split('=')[1]))
-    file = file[-1]
+    # file = Path(
+    #     Path.cwd(),
+    #     'lightning_logs',
+    #     'default',
+    #     f'version_{version}',
+    #     'checkpoints',
+    # ).glob('*.ckpt')
+    # file = sorted(list(file), key=lambda x: int(x.name.split('-')[0].split('=')[1]))
+    # file = file[-1]
+    file = 'model.ckpt'
 
     state_dict = torch.load(file)['state_dict']
     new_state = OrderedDict()
