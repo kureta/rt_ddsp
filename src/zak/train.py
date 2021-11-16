7f9f90ce046b475a8014a0036a22b1fcb54bebe1
kureta <skureta@gmail.com>
2021-11-16 14:04:42 +0300

    Use wandb for logging experiments

--- src/zak/train.py
+++ src/zak/train.py
@@ -1,11 +1,17 @@
 import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
 import torch
+import wandb
+from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
 
-from zak.model import Decoder
 from zak.dataset import Parameters
 from zak.loss import MSSLoss
+from zak.model import Decoder
+
+wandb_logger = WandbLogger(project="zak-ddsp", log_model='all')
+
+columns = ['original', 'generated']
+samples_table = wandb.Table(columns)
 
 
 # TODO: Noise goes to zero during training
@@ -27,12 +33,6 @@ class Zak(pl.LightningModule):
             },
         }
 
-    def on_epoch_start(self) -> None:
-        if self.current_epoch == 0:
-            f0 = torch.randn(1, 1, 1)
-            amp = torch.randn(1, 1, 1)
-            self.logger.experiment.add_graph(Decoder(), (f0, amp))
-
     def training_step(self, params: Parameters, _batch_idx: int) -> torch.Tensor:
         audio, pitch, loudness = params
         x_hat = self.model(pitch.permute(0, 2, 1), loudness.permute(0, 2, 1))
@@ -45,15 +45,6 @@ class Zak(pl.LightningModule):
         loss = self.loss(x_hat, audio)
 
         self.log('train_loss', loss)
-        self.logger.experiment.add_histogram(
-            'impulse', self.model.reverb.ir, self.global_step
-        )
-        self.logger.experiment.add_histogram(
-            'noise/weight', self.model.controller.dense_filter.weight, self.global_step
-        )
-        self.logger.experiment.add_histogram(
-            'noise/bias', self.model.controller.dense_filter.bias, self.global_step
-        )
         return loss
 
     def validation_step(self, params: Parameters, batch_idx: int) -> None:
@@ -62,18 +53,10 @@ class Zak(pl.LightningModule):
 
         # Log 4 examples to tensorboard
         for i in range(4):
-            self.logger.experiment.add_audio(
-                f'{batch_idx}-{i}',
-                x_hat[i].unsqueeze(1),
-                self.global_step,
-                sample_rate=48000,
-            )
-            self.logger.experiment.add_audio(
-                f'{batch_idx}-{i}-orig',
-                audio[i].unsqueeze(1),
-                self.global_step,
-                sample_rate=48000,
-            )
+            samples_table.add_data(wandb.Audio(audio[i].squeeze().cpu(), 48000, f':step {batch_idx}'),
+                         wandb.Audio(x_hat[i].squeeze().cpu(), 48000, f':step {batch_idx}'))
+
+        wandb_logger.experiment.log({'samples': samples_table})
 
 
 def main() -> None:
@@ -82,11 +65,11 @@ def main() -> None:
         dataset, batch_size=4, shuffle=True, num_workers=4
     )
     model = Zak()
-    logger = TensorBoardLogger('lightning_logs', default_hp_metric=False)
+    wandb_logger.watch(model, log="all")
     trainer = pl.Trainer(
         gpus=1,
         limit_val_batches=1,
-        logger=logger,
+        logger=wandb_logger,
         log_every_n_steps=30,
         gradient_clip_val=3.0,
         val_check_interval=0.2,
